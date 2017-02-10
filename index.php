a9c309aecebc676e9d1040cd534025458a725dcc
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-10 09:44:39 -0200

    Utilizando bootstrap na página inicial!

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <html>
-	<?php $nome = "Guilherme"; ?>
-	Nova loja do <?php echo $nome; ?>
+<head>
+	<meta charset="utf-8">
+	<title>Minha loja</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<div class="principal">
+			<h1>Bem vindo"</h1>
+		</div>
+	</div>
+</body>
 </html>

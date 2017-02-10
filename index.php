c173a85ca26f7a22ccedbd447adce5d8b70e81e0
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-10 09:49:25 -0200

    Usando loja.css no index.php

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 	<meta charset="utf-8">
 	<title>Minha loja</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/loja.css">
 </head>
 <body>
 	<div class="container">
 		<div class="principal">
-			<h1>Bem vindo"</h1>
+			<h1>Bem vindo</h1>
 		</div>
 	</div>
 </body>

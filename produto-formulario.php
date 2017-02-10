fe0dc945fc4a3aa42c5cac017704abce7b6085a4
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-10 09:31:30 -0200

    Criação do formulário de adiconar produto

---
+++ produto-formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="adiciona-produto.php">
+	Nome :
+	<input type="text" name="nome"><br>
+	Preço : 
+	<input type="number" name="preco"><br>
+	<input type="submit" value="Cadastrar">
+</form>
+
+</body>
+</html>

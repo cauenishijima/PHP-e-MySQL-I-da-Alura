67effe5460d9c28ea18000b9864a23a4c2b7ce72
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-16 14:17:08 -0200

    Melhoria no estilo das páginas com a utilização de algumas classes do bootstrap

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,36 +1,36 @@
-<?php include("cabecalho.php"); ?>
+<?php 
+	include("cabecalho.php"); 
+	include("conecta.php");
 
-	<?php 
 
-		function inserirProduto($conexao, $nome, $preco){
-			$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}',{$preco})";
-			return mysqli_query($conexao, $query);
-		}
+	function inserirProduto($conexao, $nome, $preco){
+		$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}',{$preco})";
+		return mysqli_query($conexao, $query);
+	}
 
-		$nome = $_GET["nome"];
-		$preco = $_GET["preco"];
+	$nome = $_GET["nome"];
+	$preco = $_GET["preco"];
 
-		$conexao = mysqli_connect('localhost','root','','loja');
+	$conexao = mysqli_connect('localhost','root','','loja');
 
-		
-		if (inserirProduto($conexao,$nome,$preco)) {
- 	?>
 
-		<p class=text-success>Produto <?php echo $nome; ?>, <?= $preco;?> adicionado com sucesso!</p>
+	if (inserirProduto($conexao,$nome,$preco)) {
+?>	
 
- 	<?php
-		} else {
-	?>
+	<p class=text-success>Produto <?php echo $nome; ?>, <?= $preco;?> adicionado com sucesso!</p>
 
-		<p class=text-danger>Produto <?php echo $nome; ?> não foi adicionado!</p>
+<?php
+	} else {
 
-	<?php
-		}
+	$msg_error = mysqli_error($conexao);
+?>
 
-		// O fechamento da conexão é opcional, o próprio PHP fecha todas as conexões no final da página
-		mysqli_close($conexao);
-	?>
+	<p class=text-danger>Produto <?php echo $nome; ?> não foi adicionado : <?= $msg_error?> </p>
 
-	
+<?php
+	}
+	// O fechamento da conexão é opcional, o próprio PHP fecha todas as conexões no final da página
+	mysqli_close($conexao);
+?>
 
 <?php include("rodape.php"); ?>

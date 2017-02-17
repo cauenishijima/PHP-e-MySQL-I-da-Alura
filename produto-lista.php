efc24576b57264baf52fec2251f609e8b46be500
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-17 08:13:05 -0200

    Mostra mensagem de produto excluido, adiciona o campo descrição, e usa um botão com um formulario através do método post para remover os produtos!

--- produto-lista.php
+++ produto-lista.php
@@ -3,13 +3,34 @@
 	include("conecta.php");
 	include("banco-produto.php");
 
-	$produtos = listaProdutos($conexao);
-
-	foreach($produtos as $produto) {
-		echo $produto['nome'] . "</br>";
+	if (array_key_exists('removido', $_GET) && $_GET['removido'] == true) {
+?>
+	<p class="alert-success">Produto removido com sucesso!</p>
+<?php
 	}
 ?>
 
+<table class="table table-striped table-bordered">
 
+	<?php
+		$produtos = listaProdutos($conexao);
+		foreach($produtos as $produto) :
+	?>
+		<tr>
+			<td><?=$produto['nome']?></td>
+			<td><?=$produto['preco']?></td>
+			<td><?=substr($produto['descricao'], 0, 40)?></td>
+			<td>
+			<form method="post" action="remove-produto.php">
+				<input type="hidden" name="id" value="<?=$produto['id']?>">
+				<button class="btn btn-danger">remover</button>
+			</form>
+		</tr>
+	<?php
+		endforeach
+	?>
 
-<?php include("rodape.php") ?>
+</table>
+
+<?php 
+	include("rodape.php");

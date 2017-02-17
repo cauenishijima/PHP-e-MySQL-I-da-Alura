a0334f3b337c12562073e80c7e3d4b2e3b52e8e5
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-17 08:20:50 -0200

    Criação do método removeProduto

--- banco-produto.php
+++ banco-produto.php
@@ -1,8 +1,13 @@
 <?php
 	include("conecta.php");
 
-	function inserirProduto($conexao, $nome, $preco){
-		$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}',{$preco})";
+	function inserirProduto($conexao, $nome, $preco, $descricao){
+		$query = "INSERT INTO produtos (nome, preco,descricao) VALUES ('{$nome}',{$preco}, '{$descricao}')";
+		return mysqli_query($conexao, $query);
+	}
+
+	function removerProduto($conexao, $id){
+		$query = "DELETE FROM produtos WHERE id = '{$id}'";
 		return mysqli_query($conexao, $query);
 	}
 

73d4830d3bcf38e72889bddf4fc608c47e7537d3
Cauê Nishijima <caue@astecainforma.com.br>
2017-02-21 08:06:28 -0300

    Mensagem de falha de segurança alterada!

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 	if (isset($_GET['falhaDeSeguranca']) && $_GET['falhaDeSeguranca'] == true):
 ?>
-		<p class="alert-danger">Usuário não esta logado!</p>
+		<p class="alert-danger">Você não tem acesso a essa funcionalidade.</p>
 <?php
 	endif;
 ?>
